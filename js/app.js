f5937d2d7e2208cace67c42f162e1788b67fdce4
Martin89PL <marcingladkowski@gmail.com>
2016-07-13 02:21:00 +0200

    game2_beta_1_2

--- js/app.js
+++ js/app.js
@@ -30,6 +30,10 @@ function game1(){
 function removeAnimals (){
     $("#imageDiv").empty();
 }
+function removeElemens (){
+    $("#icons1").empty();
+    $("#icons2").empty();
+}
 
 var buttons = $('button');
 //add index for each button
@@ -103,19 +107,47 @@ var oper = 0;
 var summary;
 
 function game2() {
-    
+    var val1Array=[];
+    var val2Array=[];
     var val1 = Math.floor((Math.random() * 10) + 1);
     var val2 = Math.floor((Math.random() * 10) + 1);
     var oper = Math.floor((Math.random() *  option.length));
-
     var container = $('#box1');
     var lbl = $('#action');
-    
     lbl.text(val1 + ' ' + option[oper] + ' ' + val2 + ' = ');
-    
     summary = (operation(val1, val2, option[oper]));
-    
     console.log('Wynik działania: ' + summary);
+//    display icons row 1
+     while(val1Array.length < val1){
+        var n =Math.floor((Math.random()*30)+1);
+            val1Array.push(n);
+    }
+    var i=0;
+
+    for (var i = 0; i < val1; i++) {
+    var img = $('<img id="icon">');
+    img.attr('src', 'images/svg2/'+val1Array[i]+'.svg');
+    img.appendTo('#icons1').css('display', 'flex').css('height', '15vh');
+}
+    if (oper == 0) {
+        $('#sign').text('+');
+    } 
+    if (oper == 1) {
+        $('#sign').text('-');
+    }
+//    display icons row 2
+     while(val2Array.length < val2){
+        var n =Math.floor((Math.random()*30)+1);
+            val2Array.push(n);
+    }
+    var i=0;
+
+    for (var i = 0; i < val2; i++) {
+    var img = $('<img id="icon">');
+    img.attr('src', 'images/svg2/'+val2Array[i]+'.svg');
+    img.appendTo('#icons2').css('display', 'flex').css('height', '15vh');
+}
+    
 
 }
 function removeNumber (){
@@ -134,6 +166,7 @@ btn.on('click', function() {
            console.log('super');
        } else {console.log('błąd');}
     removeNumber();
+    removeElemens();
     game2(); 
 
 });
